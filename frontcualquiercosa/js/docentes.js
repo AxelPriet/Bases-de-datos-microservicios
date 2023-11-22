c300991af275641eec9ad225243ea81c5d288f5e
XxCamilo14xX <colemus@uniboyaca.edu.co>
2023-11-22 12:46:43 -0500

    hola

--- frontcualquiercosa/js/docentes.js
+++ frontcualquiercosa/js/docentes.js
@@ -1,62 +1,118 @@
 $(document).ready(() =>{
-
-let docentes = [];
-
-$.ajax({
-    url: 'http://127.0.0.1:8081/docentes',
-    method: 'get'
-}).done((response)=>{
-    estudiantes = response;
-    console.log(docentes[0]);
-    Array.array.forEach(element => {
-        document.body.innerHTML += '<h2>'+item.nombre+'</h2>';  
-    });
-}).fail((err)=>{
-    console.error(err);
-});
-
-
-
-$.ajax({
-    url: 'http://127.0.0.1:8081/docentes',
-    method: 'post',
-    data:{nombre: 'Pepe', codigo: '55822508', ocupacion: 'Ingenieria en sistemas'}
-}).done((response)=>{
-    console.log(response);
-}).fail((err)=>{
-    console.error(err);
-});
-
-$.ajax({
-    url: 'http://127.0.0.1:8081/docentes',
-    method: 'put',
-    data:{nombre: 'Pepe 2', codigo: '55822508', ocupacion: 'Ingenieria en sistemas'}
-}).done((response)=>{
-    console.log(response);
-}).fail((err)=>{
-    console.error(err);
-});
-
-$.ajax({
-    url: 'http://127.0.0.1:8081/docentes',
-    method: 'Delete',
-}).done((response)=>{
-    console.log(response);
-}).fail((err)=>{
-    console.error(err);
-});
-
-function pruebaClick(id) {
-    alert('ok: ' + id);
-    $.ajax({
-        url: 'http://127.0.0.1:8081/estudiantes/' + id,
-        method: 'get',
-    }).done((response) => {
-        console.log(response);
-    }).fail((err) => {
-        console.error(err);
-    });
-}
-
-
-});
+    let docentes = [];
+    function crearDocente(nombre, codigo, ocupacion) {
+        $.ajax({
+            url: 'http://127.0.0.1:8081/docentes',
+            method: 'post',
+            data:{nombre: nombre, codigo: codigo, ocupacion: ocupacion}
+        }).done((response)=>{
+            console.log(response);
+        }).fail((err)=>{
+            console.error(err);
+        });
+    }
+    function leerDocente(codigo) {
+        $.ajax({
+            url: 'http://127.0.0.1:8081/docentes/' + codigo,
+            method: 'get',
+        }).done((response)=>{
+            console.log(response);
+        }).fail((err)=>{
+            console.error(err);
+        });
+    }
+    function actualizarDocente(codigo, nombre, ocupacion) {
+        $.ajax({
+            url: 'http://127.0.0.1:8081/docentes',
+            method: 'put',
+            data:{nombre: nombre, codigo: codigo, ocupacion: ocupacion}
+        }).done((response)=>{
+            console.log(response);
+        }).fail((err)=>{
+            console.error(err);
+        });
+    }
+    function eliminarDocente(codigo) {
+        var cursos = obtenerCursosDocente(codigo);
+        if (cursos.length > 0) {
+            console.error('No se puede eliminar un docente que tenga cursos asociados');
+        } else {
+            $.ajax({
+                url: 'http://127.0.0.1:8081/docentes',
+                method: 'Delete',
+            }).done((response)=>{
+                console.log(response);
+            }).fail((err)=>{
+                console.error(err);
+            });
+        }
+    }
+    $(document).ready(() =>{
+        let docentes = [];
+        function crearCurso(codigo, nombre, codigoDocente) {
+            $.ajax({
+                url: 'http://127.0.0.1:8081/cursos',
+                method: 'post',
+                data:{codigo: codigo, nombre: nombre, codigoDocente: codigoDocente}
+            }).done((response)=>{
+                console.log(response);
+            }).fail((err)=>{
+                console.error(err);
+            });
+        }
+        function leerCurso(codigo) {
+            $.ajax({
+                url: 'http://127.0.0.1:8081/cursos/' + codigo,
+                method: 'get',
+            }).done((response)=>{
+                console.log(response);
+            }).fail((err)=>{
+                console.error(err);
+            });
+        }
+        function actualizarCurso(codigo, nombre, codigoDocente) {
+            $.ajax({
+                url: 'http://127.0.0.1:8081/cursos',
+                method: 'put',
+                data:{codigo: codigo, nombre: nombre, codigoDocente: codigoDocente}
+            }).done((response)=>{
+                console.log(response);
+            }).fail((err)=>{
+                console.error(err);
+            });
+        }
+        function eliminarCurso(codigo) {
+            $.ajax({
+                url: 'http://127.0.0.1:8081/cursos',
+                method: 'Delete',
+            }).done((response)=>{
+                console.log(response);
+            }).fail((err)=>{
+                console.error(err);
+            });
+        }
+        function obtenerCursosDocente(codigoDocente) {
+            $.ajax({
+                url: 'http://127.0.0.1:8081/docentes/' + codigoDocente + '/cursos',
+                method: 'get',
+            }).done((response)=>{
+                console.log(response);
+            }).fail((err)=>{
+                console.error(err);
+            });
+        }
+        function obtenerDocentesCurso(codigoCurso) {
+            $.ajax({
+                url: 'http://127.0.0.1:8081/cursos/' + codigoCurso + '/docentes',
+                method: 'get',
+            }).done((response)=>{
+                console.log(response);
+            }).fail((err)=>{
+                console.error(err);
+            });
+        }
+        function pruebaClick(id) {
+            alert('ok: ' + id);
+            leerDocente(id);
+        }
+    });})
