6f6645008afd764beee4ef9e9557659621f7b2ea
XxCamilo14xX <colemus@uniboyaca.edu.co>
2023-11-21 19:46:59 -0500

    h

---
+++ BE_microservicios/app/Http/Middleware/CorsMiddleware.php
@@ -0,0 +1,14 @@
+public function handle($request, Closure $next)
+$headers •
+• Access -Control -Al low-Methods
+' Access -Control -Allow-Headers•
+Access -Control -Allow-Origin '
+Access -Control '
+Access -Control -Max -Age
+'POST, GET,
+'true•,
+' 864ee' ,
+OPTIONS,
+PUT,
+DELETE ' ,
+'Content-Type, Authorization, X-Reque
